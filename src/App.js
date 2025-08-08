3b60a4e6f20614f93d0c7b21c47431e293574588
kunal <kunalgautam200@gmail.com>
2025-08-08 18:13:50 +0530

    routes and handler

--- src/App.js
+++ src/App.js
@@ -5,10 +5,26 @@ const app = express();
 // basically what the server return whenever user target on port:3000
 // After adding the route with handler funct that means, when the request is targeting that particular path then only server send the response. 
 // structure: app.use('route',requestHandlerFunction);
-app.use('/test',(req,res)=>{
+
+// get call 
+app.get('/test',(req,res)=>{
     console.log('request',req===global);
-    res.send('Testing from the server');
+    res.send({firstName:'kunal',lastName:'gautam'});
 });
+
+// post call
+
+app.post('/test',(req,res)=>{
+    // console.log('request',req===global);
+        res.send({firstName:'Ankit',lastName:'gautam'});
+
+});
+app.delete('/test',(req,res)=>{
+    // console.log('request',req===global);
+        res.send('Data has been deleted');
+
+});
+
 // Listen all the request at port : 3000
 app.listen(3000,()=>{
     console.log('Server is running');

ae76753cbf1dcd7173f7b23fa077b751ab3c33b3
kunal <kunalgautam200@gmail.com>
2025-08-10 22:42:23 +0530

    Individual calls of router path

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ Testing multi route handle inside the single route.
 
 Point: 
 if the multi handler doesn't have the next() function and if any of the handler doesn't send the response then, it through the error. cause it still looking for the response from the next handler. 
+:- Instead of calling multi route handler inside the single route func, we can create diff route func of the same route. 
 
 */
 let rh1 = (req, res, next) => {
     console.log('request-from handler1');
-    // console.log('request-1 params', req.params);
-    // console.log('request-1 query', req.query);
+    console.log('request-1 params', req.params);
+    console.log('request-1 query', req.query);
 
 
     next();
@@ -22,22 +23,23 @@ let rh1 = (req, res, next) => {
 }
 let rh2 = (req, res, next) => {
     console.log('request-from handler2');
-    // console.log('request-2 params', req.params);
-    // console.log('request-2 query', req.query);
+    console.log('request-2 params', req.params);
+    console.log('request-2 query', req.query);
 
 
-    // res.send({ firstName: 'kunal', lastName: 'gautam' });
+    res.send({ firstName: 'kunal', lastName: 'gautam' });
     next();
 }
 let rh3 = (req, res, next) => {
     console.log('request-from handler3');
-    // console.log('request-3 params', req.params);
+    console.log('request-3 params', req.params);
     console.log('request-3 query', req.query);
 
 
     res.send({ firstName: 'kunal', lastName: 'gautam' });
 }
-app.get('/test/:id/:work/:age', [[rh1, rh2], [rh3]]);
+app.get('/test/:id/:work/:age', rh1);
+app.get('/test/:id/:work/:age', rh2);
 
 // Listen all the request at port : 3000
 app.listen(3000, () => {

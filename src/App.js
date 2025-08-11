949e7cd2907eca8098b2d2021abe58feb2887ef4
kunal <kunalgautam200@gmail.com>
2025-08-11 23:24:14 +0530

    right way to connect the clustor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
-require('./config/database.js');
-// Listen all the request at port : 3000
-app.listen(3000, () => {
-    console.log('Server is running');
-});
+const { connectDB } = require('./config/database.js');
+
+connectDB().
+    then(() => {
+        // first connect to the DB
+        console.log('Connected! to the DB');
+
+        // then listen to the port : 3000
+        app.listen(3000, () => {
+            console.log('Server is running');
+        });
+    }).
+    catch((err) => console.log('err', err))
--- src/config/database.js
+++ src/config/database.js
@@ -5,11 +5,15 @@ mongoose.connect('mongodb+srv://MO-cluster:oDVhIyh8q8hphOV0@mo-cluster.dwdljhy.m
 
 This above URL return the promise hence it's good practice to wrap this function in async func.
 
+- right to to setup the connection with the clustor/db is that,
+ - First connect to db
+ - then connect to the port at which the application is running. 
+
 */
 
 const connectDB = async () => {
     await mongoose.connect('mongodb+srv://MO-cluster:oDVhIyh8q8hphOV0@mo-cluster.dwdljhy.mongodb.net/');
 }
-connectDB().
-    then(() => console.log('Connected! to the DB')).
-    catch((err) => console.log('err', err))
+module.exports = {
+    connectDB: connectDB
+}

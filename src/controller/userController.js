3d766c155a532c4510b876b242e550e9bfa8160a
kunal <kunalgautam200@gmail.com>
2026-08-02 12:38:12 +0530

    Added the changes for feed and connections

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,7 +1,7 @@
 const ConnectionRequestModel = require('../model/connectionRequest');
 const { User } = require('../model/user');
 
-const UserAllowedData = ["firstName", "lastName", "gender", "age", "skills", "profilePic"];
+const UserAllowedData = ["firstName", "lastName", "gender", "age", "skills", "profilePic", "education"];
 const userController = {
     userFeeds: async (req, res) => {
         try {
@@ -17,22 +17,41 @@ const userController = {
                 ]
             }).select("fromUserId toUserId");
             const hideUsersFromFeed = new Set();
-            // console.log('hasUsersFromFeed',hideUsersFromFeed);
             findAlreadySentRequest.forEach((req) => {
                 hideUsersFromFeed.add(req.fromUserId.toString());
                 hideUsersFromFeed.add(req.toUserId.toString());
 
-            })
+            });
+            const findsUserFeedCount = await User.countDocuments({
+                $and: [
+                    { _id: { $nin: Array.from(hideUsersFromFeed) } },
+                    { _id: { $ne: loggedInUser._id } }
+                ]
+            });
+            console.log('totalCount', findsUserFeedCount);
             const findsUserFeed = await User.find({
                 $and: [
                     { _id: { $nin: Array.from(hideUsersFromFeed) } },
                     { _id: { $ne: loggedInUser._id } }
                 ]
             }).select(UserAllowedData).skip(skip).limit(limit);
-            // console.log(hideUsersFromFeed);
-            // const usersData = await User.find({});
-            // if (!usersData) throw new Error('Users data not appear');
-            res.status(200).send(findsUserFeed);
+            if (findsUserFeed.length <= 0) {
+                return res.status(200).json({
+                    message: 'No Feed to shows',
+                    data: [],
+                    totalCount: findsUserFeedCount,
+                    page: page,
+                    limit: limit
+                });
+            }
+            // res.status(200).send(findsUserFeed);
+            res.status(200).json({
+                message: 'User feed',
+                totalCount: findsUserFeedCount,   // e.g. 12
+                page: page,                      // e.g. 1
+                limit: limit,                     // e.g. 10
+                data: findsUserFeed               // actual user records
+            });
         } catch (error) {
             res.status(500).json({ error: error.message });
         }
@@ -46,7 +65,7 @@ const userController = {
             let skip = (page - 1) * limit;
             let loggedInUser = req.user;
             // Get total count first
-            const totalItems = await ConnectionRequestModel.countDocuments({
+            const totalPendingRequestCount = await ConnectionRequestModel.countDocuments({
                 toUserId: loggedInUser._id,
                 status: 'interested'
             });
@@ -59,18 +78,18 @@ const userController = {
                 return res.status(200).json({
                     message: 'No pending connection request',
                     data: [],
-                    totalItems,
-                    page,
-                    limit
+                    totalCount: totalPendingRequestCount,
+                    page: page,
+                    limit: limit
                 });
             }
 
             res.status(200).json({
                 message: 'Following are the pending requests',
                 data: loggedInUserPendingRequest,
-                totalItems,
-                page,
-                limit
+                totalCount: totalPendingRequestCount,
+                page: page,
+                limit: limit
             });
         } catch (error) {
             res.status(500).json({
@@ -93,17 +112,22 @@ const userController = {
                 ]
             }).populate('fromUserId', UserAllowedData).
                 populate('toUserId', UserAllowedData);
-            //    send only the connected user data
-            const data = myConnections.map((item) => {
-                if (item.fromUserId._id.toString() === loggedInUser._id.toString()) {
-                    return item.toUserId;
-                }
-                return item.fromUserId;
-            })
+            // Include both connected user and requestId
+            const data = myConnections.map(item => {
+                const connectedUser = item.fromUserId._id.toString() === loggedInUser._id.toString()
+                    ? item.toUserId
+                    : item.fromUserId;
+
+                return {
+                    requestId: item._id,   // connection request ID
+                    user: connectedUser    // connected user data
+                };
+            });
+
             res.status(200).json({
                 message: 'Logged in connections',
-                data: data
-            })
+                data
+            });
 
         } catch (error) {
             res.status(500).json({
@@ -115,7 +139,7 @@ const userController = {
 
     userRequestHistory: async (req, res) => {
         try {
-            const userId = req.user._id;
+            const loggedInUserId = req.user._id;
             const [
                 ignoredByMe,
                 sentInterested,
@@ -125,25 +149,25 @@ const userController = {
 
                 // ✅ FIXED: ignored is fromUserId
                 ConnectionRequestModel.find({
-                    fromUserId: userId,
+                    fromUserId: loggedInUserId,
                     status: "ignored"
                 }).populate("toUserId", UserAllowedData),
 
                 // interested sent
                 ConnectionRequestModel.find({
-                    fromUserId: userId,
+                    fromUserId: loggedInUserId,
                     status: "interested"
                 }).populate("toUserId", UserAllowedData),
 
                 // rejected by logged-in user (incoming)
                 ConnectionRequestModel.find({
-                    toUserId: userId,
+                    toUserId: loggedInUserId,
                     status: "rejected"
                 }).populate("fromUserId", UserAllowedData),
 
                 // user got rejected
                 ConnectionRequestModel.find({
-                    fromUserId: userId,
+                    fromUserId: loggedInUserId,
                     status: "rejected"
                 }).populate("toUserId", UserAllowedData)
 

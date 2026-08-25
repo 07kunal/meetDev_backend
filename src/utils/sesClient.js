25e41ddf00a1711ef0822417c048b7c7958a2e3f
kunal <kunalgautam200@gmail.com>
2026-08-25 22:57:10 +0530

    added IAm role

--- src/utils/sesClient.js
+++ src/utils/sesClient.js
@@ -2,10 +2,6 @@ const { SESClient } = require("@aws-sdk/client-ses");
 // Set the AWS Region.
 const REGION = process?.env?.AWS_REGION;
 // Create SES service object.
-const sesClient = new SESClient({ region: REGION, credentials:{
-    accessKeyId: process?.env?.AWS_ACCESS_KEY,
-    secretAccessKey: process?.env?.AWS_SECRET_KEY
-
-} });
+const sesClient = new SESClient({ region: REGION});
 module.exports = { sesClient };
 // snippet-end:[ses.JavaScript.createclientv3]

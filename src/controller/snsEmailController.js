b83c90203557d6fc5cbd4eeb5c0ee613d2bf1545
kunal <kunalgautam200@gmail.com>
2026-09-01 22:19:02 +0530

    added the changes

--- src/controller/snsEmailController.js
+++ src/controller/snsEmailController.js
@@ -1,6 +1,22 @@
 const snsEmailController = {
     snsNotification: async (req, res) => {
         console.log('req--sns', req);
+        const messageType = req.headers["x-amz-sns-message-type"];
+
+        console.log("SNS Type:", messageType);
+        console.log("SNS Body:", req.body);
+
+        if (messageType === "SubscriptionConfirmation") {
+            console.log("Confirm this subscription:");
+            console.log(req.body.SubscribeURL);
+        }
+
+        if (messageType === "Notification") {
+            const message = JSON.parse(req.body.Message);
+
+            console.log("SES Event:");
+            console.log(message);
+        }
         res.status(200).json({
             message: "Email sent successfully"
         });
